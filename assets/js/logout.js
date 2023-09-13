26a2f9b9aac2616fb350176c9dca54c3e52fc404
cumauu73 <cumauu73@icloud.com>
2023-09-13 12:35:27 -0500

    index

--- assets/js/logout.js
+++ assets/js/logout.js
@@ -3,5 +3,5 @@ const logoutButton = document.getElementById("logout");
 // Add an event listener to the logout button
 logoutButton.addEventListener("click", function() {
     // Redirect to the login page
-    window.location.href = "login.html";
+    window.location.href = "index.html";
 });
